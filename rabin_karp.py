a52c8c8052971e9f113975b253f51ec17bbc42d6
Egor <catsmeowmeoww@gmail.com>
2021-05-02 13:55:54 +0300

    update

--- rabin_karp.py
+++ rabin_karp.py
@@ -1,4 +1,12 @@
 def toint(nucleobase):
+    '''
+    A - Adenine
+    G - Guanine
+    C - Cytosine
+    T - Thymine
+
+    return number for each nucleobase or rise ValueError
+    '''
     if nucleobase == 'A':
         return 0
     elif nucleobase == 'G':
@@ -11,33 +19,52 @@ def toint(nucleobase):
         raise ValueError("Check input nucleobases")
 
 
-def get_hash(p, l):
+def get_hash(p, l, r):
+    '''
+    A - base of n-number system
+    l - left border
+    r - right border (not included)
+    n - hash
+    
+    return hash of slice of string
+    '''
     global A
 
     n = 0
-
-    for i in range(l):
+    for i in range(l, r):
         n = n * A + toint(p[i])
     return n
 
 
 def RabinKarp(t, p):
+    '''
+    A - base of n-number system
+    N_hash - hash of slice of the main string
+    M_hash - hash of substring
+
+    return index from which substring starting in the main string
+    or 'Not found' if no substring in the main string
+    '''
     global A
 
     A = 4
     len_t = len(t)
     len_p = len(p)
-    M_hash = get_hash(p, len_p)
-    N_hash = get_hash(t, len_p)
-    n = A**(len(p) - 1)
+    M_hash = get_hash(p, 0, len_p)
     for i in range(len_t - len_p):
+        N_hash = get_hash(t, i, len_p + i)
         if M_hash == N_hash:
             return i
-        N_hash = (N_hash % n) * A + toint(t[len_p + i])
+        #N_hash = (N_hash % n) * A + toint(t[len_p + i])
+        
     return 'Not found'
 
 
 def main():
+    '''
+    t - main string
+    p - substring which we should find in the main string
+    '''
     with open('inputs.txt', 'r') as inputs:
         t = inputs.readline().strip()
         p = inputs.readline().strip()
